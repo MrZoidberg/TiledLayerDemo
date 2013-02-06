7295d8f0b73c61ece17ac4b9c75b70eb516acd0d
MrZoidberg <mihail.merkulov@gmail.com>
2013-02-06 15:27:24 +0200

    Initial Commit

---
+++ TiledLayerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TiledLayerDemo
+//
+//  Created by Mykhaylo Merkulov on 06.02.13.
+//  Copyright (c) 2013 Mikhail Merkulov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TiledLayerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TiledLayerDemo
+//
+//  Created by Mykhaylo Merkulov on 06.02.13.
+//  Copyright (c) 2013 Mikhail Merkulov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
